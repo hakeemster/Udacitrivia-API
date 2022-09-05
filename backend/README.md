693f3fdf0489d27350551bbf9dd64ec8c343b66f
Abdulhakeem Abdulrafeeu <abdulrafeeu279@gmail.com>
2022-09-05 11:47:55 +0100

    implemented assessment requirements

--- backend/README.md
+++ backend/README.md
@@ -71,7 +71,7 @@ One note before you delve into your tasks: for each endpoint, you are expected t
 
 You will need to provide detailed documentation of your API endpoints including the URL, request parameters, and the response body. Use the example below as a reference.
 
-### Documentation Example
+<!-- ### Documentation Example
 
 `GET '/api/v1.0/categories'`
 
@@ -88,7 +88,189 @@ You will need to provide detailed documentation of your API endpoints including 
   "5": "Entertainment",
   "6": "Sports"
 }
+``` -->
+
+## API Documentation
+
+### Getting Started
+- Base URL: At present this app can only be run locally and is not hosted as a base URL. The backend app is hosted at the default, `http://127.0.0.1:5000/`, which is set as a proxy in the frontend configuration. 
+- Authentication: This version of the application does not require authentication or API keys. 
+
+### Error Handling
+Errors are returned as JSON objects in the following format:
 ```
+{
+    "success": False, 
+    "error": 400,
+    "message": "bad request"
+}
+```
+The API will return three error types when requests fail:
+- 400: Bad Request
+- 404: Resource Not Found
+- 422: Not Processable 
+- 500: Server Error
+
+### Endpoints 
+
+#### GET /categories 
+Fetches a dictionary of all available categories.
+- Request arguments: *None*
+- Returns: An object with a single key, `categories`, that contains an object of `id: category_string` key: value pairs.
+- Sample request: `curl http://127.0.0.1:5000/categories`
+- Sample response:
+
+```json
+{
+  "categories": {
+    "1": "Science", 
+    "2": "Art", 
+    "3": "Geography", 
+    "4": "History", 
+    "5": "Entertainment", 
+    "6": "Sports"
+  }, 
+  "success": true
+}
+```
+
+#### GET /questions?page=<page_number> 
+Fetches a paginated dictionary of questions of all available categories.
+- Request arguments (optional): *page:int*
+- Example response:  
+
+ ```json
+{
+  "categories": {
+    "1": "Science", 
+    "2": "Art", 
+    "3": "Geography", 
+    "4": "History", 
+    "5": "Entertainment", 
+    "6": "Sports"
+  }, 
+  "current_category": null, 
+  "questions": [
+    {
+      "answer": "Maya Angelou", 
+      "category": 4, 
+      "difficulty": 2, 
+      "id": 5, 
+      "question": "Whose autobiography is entitled 'I Know Why the Caged Bird Sings'?"
+    },  
+    {
+      "answer": "Escher", 
+      "category": 2, 
+      "difficulty": 1, 
+      "id": 16, 
+      "question": "Which Dutch graphic artist\u2013initials M C was a creator of optical illusions?"
+    }
+  ], 
+  "success": true, 
+  "total_questions": 2
+}
+```
+
+#### DELETE /questions/<question_id>
+Delete an existing question with the given id
+- Request argument(s): *question_id:int*
+- Sample request: `curl -X DELETE http://127.0.0.1:5000/questions/14`
+- Example response: 
+
+```json
+{
+  "deleted": "28", 
+  "success": true
+}
+```
+
+#### POST /questions
+Adds a new question to the repository of available questions.
+- Request argument: *None*
+- Request body: *{question:string, answer:string, difficulty:int, category:string}*
+- Example response:
+
+```json
+{
+  "created": 29, 
+  "success": true
+}
+```
+
+#### POST /questions/search
+Fetches all questions where a substring matches the inputted search term (not case-sensitive)
+- Request argument(s): *None*
+- Request body: *{searchTerm:string}*
+- Example response:
+
+```json
+{
+  "current_category": null, 
+  "questions": [
+    {
+      "answer": "Lisbon", 
+      "category": 2, 
+      "difficulty": 1, 
+      "id": 29, 
+      "question": "What is the capital of Portugal?"
+    }
+  ], 
+  "success": true, 
+  "total_questions": 1
+}
+```
+
+#### GET /categories/<int:category_id>/questions
+Fetches a dictionary of questions for the specified category
+- Request argument(s): *category_id:int*
+- Sample request: `curl http://127.0.0.1:5000/categories/1/questions`
+- Example response:
+
+```json
+{
+  "current_category":1, 
+  "questions": [
+    {
+      "answer": "The Liver", 
+      "category": 1, 
+      "difficulty": 4, 
+      "id": 20, 
+      "question": "What is the heaviest organ in the human body?"
+    }, 
+    {
+      "answer": "Alexander Fleming", 
+      "category": 1, 
+      "difficulty": 3, 
+      "id": 21, 
+      "question": "Who discovered penicillin?"
+    }, 
+  ], 
+  "success": true, 
+  "total_questions": 2
+}
+```
+
+#### POST /quizzes
+Fetches one random question within a specified category. Previously asked questions are not repeated. 
+- Request argument(s): *None*
+- Request body: *{previous_questions: arr, quiz_category: {id:int, type:string}}*
+- Example response: 
+
+```json
+{
+  "question": {
+    "answer": "The Liver", 
+    "category": 1, 
+    "difficulty": 4, 
+    "id": 20, 
+    "question": "What is the heaviest organ in the human body?"
+  }, 
+  "success": true
+}
+``` 
+
+
+
 
 ## Testing
 
@@ -102,3 +284,171 @@ createdb trivia_test
 psql trivia_test < trivia.psql
 python test_flaskr.py
 ```
+---
+
+## API Reference
+
+### Getting Started
+- Base URL: At present this app can only be run locally and is not hosted as a base URL. The backend app is hosted at the default, `http://127.0.0.1:5000/`, which is set as a proxy in the frontend configuration. 
+- Authentication: This version of the application does not require authentication or API keys. 
+
+### Error Handling
+Errors are returned as JSON objects in the following format:
+```
+{
+    "success": False, 
+    "error": 400,
+    "message": "bad request"
+}
+```
+The API will return three error types when requests fail:
+- 400: Bad Request
+- 404: Resource Not Found
+- 422: Not Processable 
+
+### Endpoints 
+#### GET /books
+- General:
+    - Returns a list of book objects, success value, and total number of books
+    - Results are paginated in groups of 8. Include a request argument to choose page number, starting from 1. 
+- Sample: `curl http://127.0.0.1:5000/books`
+
+``` {
+  "books": [
+    {
+      "author": "Stephen King",
+      "id": 1,
+      "rating": 5,
+      "title": "The Outsider: A Novel"
+    },
+    {
+      "author": "Lisa Halliday",
+      "id": 2,
+      "rating": 5,
+      "title": "Asymmetry: A Novel"
+    },
+    {
+      "author": "Kristin Hannah",
+      "id": 3,
+      "rating": 5,
+      "title": "The Great Alone"
+    },
+    {
+      "author": "Tara Westover",
+      "id": 4,
+      "rating": 5,
+      "title": "Educated: A Memoir"
+    },
+    {
+      "author": "Jojo Moyes",
+      "id": 5,
+      "rating": 5,
+      "title": "Still Me: A Novel"
+    },
+    {
+      "author": "Leila Slimani",
+      "id": 6,
+      "rating": 5,
+      "title": "Lullaby"
+    },
+    {
+      "author": "Amitava Kumar",
+      "id": 7,
+      "rating": 5,
+      "title": "Immigrant, Montana"
+    },
+    {
+      "author": "Madeline Miller",
+      "id": 8,
+      "rating": 5,
+      "title": "CIRCE"
+    }
+  ],
+"success": true,
+"total_books": 18
+}
+```
+
+#### POST /books
+- General:
+    - Creates a new book using the submitted title, author and rating. Returns the id of the created book, success value, total books, and book list based on current page number to update the frontend. 
+- `curl http://127.0.0.1:5000/books?page=3 -X POST -H "Content-Type: application/json" -d '{"title":"Neverwhere", "author":"Neil Gaiman", "rating":"5"}'`
+```
+{
+  "books": [
+    {
+      "author": "Neil Gaiman",
+      "id": 24,
+      "rating": 5,
+      "title": "Neverwhere"
+    }
+  ],
+  "created": 24,
+  "success": true,
+  "total_books": 17
+}
+```
+#### DELETE /books/{book_id}
+- General:
+    - Deletes the book of the given ID if it exists. Returns the id of the deleted book, success value, total books, and book list based on current page number to update the frontend. 
+- `curl -X DELETE http://127.0.0.1:5000/books/16?page=2`
+```
+{
+  "books": [
+    {
+      "author": "Gina Apostol",
+      "id": 9,
+      "rating": 5,
+      "title": "Insurrecto: A Novel"
+    },
+    {
+      "author": "Tayari Jones",
+      "id": 10,
+      "rating": 5,
+      "title": "An American Marriage"
+    },
+    {
+      "author": "Jordan B. Peterson",
+      "id": 11,
+      "rating": 5,
+      "title": "12 Rules for Life: An Antidote to Chaos"
+    },
+    {
+      "author": "Kiese Laymon",
+      "id": 12,
+      "rating": 1,
+      "title": "Heavy: An American Memoir"
+    },
+    {
+      "author": "Emily Giffin",
+      "id": 13,
+      "rating": 4,
+      "title": "All We Ever Wanted"
+    },
+    {
+      "author": "Jose Andres",
+      "id": 14,
+      "rating": 4,
+      "title": "We Fed an Island"
+    },
+    {
+      "author": "Rachel Kushner",
+      "id": 15,
+      "rating": 1,
+      "title": "The Mars Room"
+    }
+  ],
+  "deleted": 16,
+  "success": true,
+  "total_books": 15
+}
+```
+#### PATCH /books/{book_id}
+- General:
+    - If provided, updates the rating of the specified book. Returns the success value and id of the modified book. 
+- `curl http://127.0.0.1:5000/books/15 -X PATCH -H "Content-Type: application/json" -d '{"rating":"1"}'`
+```
+{
+  "id": 15,
+  "success": true
+}
--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -1,4 +1,5 @@
 import os
+from unicodedata import category
 from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
@@ -8,24 +9,63 @@ from models import setup_db, Question, Category
 
 QUESTIONS_PER_PAGE = 10
 
+# Pagination method
+def paginate_questions(request, selection):
+    page = request.args.get("page", 1, type=int)
+    start = (page - 1) * QUESTIONS_PER_PAGE
+    end = start + QUESTIONS_PER_PAGE
+
+    questions = [question.format() for question in selection]
+    current_questions = questions[start:end]
+
+    return current_questions
+
+
 def create_app(test_config=None):
-    # create and configure the app
+
     app = Flask(__name__)
     setup_db(app)
+    # CORS(app)
 
     """
     @TODO: Set up CORS. Allow '*' for origins. Delete the sample route after completing the TODOs
     """
+    cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
     """
     @TODO: Use the after_request decorator to set Access-Control-Allow
     """
+    @app.after_request
+    def after_request(response):
+        response.headers.add(
+            "Access-Control-Allow-Headers", "Content-Type,Authorization,true"
+        )
+        response.headers.add(
+            "Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS"
+        )
+        return response
 
     """
     @TODO:
     Create an endpoint to handle GET requests
     for all available categories.
     """
+    @app.route("/categories")
+    def get_all_categories():
+
+        # get all categories
+        categories = Category.query.all()
+
+        categoriesDict = {}
+
+        for category in categories:
+            categoriesDict[category.id] = category.type
+
+        return jsonify({
+            'success': True,
+            'categories': categoriesDict
+        })
+
 
 
     """
@@ -34,17 +74,70 @@ def create_app(test_config=None):
     including pagination (every 10 questions).
     This endpoint should return a list of questions,
     number of total questions, current category, categories.
+    """
+    @app.route('/questions')
+    def retrieve_questions():
+        try:
+            # get questions
+            selection = Question.query.order_by(Question.id).all()
+            current_questions = paginate_questions(request, selection)
 
+            if (len(current_questions) == 0):
+                abort(404)
+
+            # get categories
+            categories = Category.query.all()
+            all_categories = {}
+            
+            for category in categories:
+                all_categories[category.id] = category.type
+
+            return jsonify({
+                'success': True,
+                'questions': current_questions,
+                'total_questions': len(Question.query.all()),
+                'categories': all_categories,
+                'current_category': None
+            })
+            
+        except Exception:
+            abort(400)
+
+    """
     TEST: At this point, when you start the application
     you should see questions and categories generated,
     ten questions per page and pagination at the bottom of the screen for three pages.
     Clicking on the page numbers should update the questions.
     """
 
+
     """
     @TODO:
     Create an endpoint to DELETE question using a question ID.
+    """
+    @app.route('/questions/<int:question_id>', methods=['DELETE'])
+    def delete_question(question_id):
+        try:
+            question = Question.query.filter(Question.id == question_id).one_or_none()
 
+            if question is None:
+                abort(404)
+
+            question.delete()
+
+            selection = Question.query.order_by(Question.id).all()
+            current_questions = paginate_questions(request, selection)
+
+            return jsonify(
+                {
+                    "success": True,
+                }
+            )
+
+        except:
+            abort(422)
+
+    """
     TEST: When you click the trash icon next to a question, the question will be removed.
     This removal will persist in the database and when you refresh the page.
     """
@@ -54,7 +147,36 @@ def create_app(test_config=None):
     Create an endpoint to POST a new question,
     which will require the question and answer text,
     category, and difficulty score.
+    """
+    @app.route('/questions', methods=['POST'])
+    def create_question():
+        body = request.get_json()
 
+        new_question = body.get("question", None)
+        new_answer = body.get("answer", None)
+        new_difficulty = body.get("difficulty", None)
+        new_category = body.get("category", None)
+
+        try:
+            question = Question(question=new_question, answer=new_answer, difficulty=new_difficulty, category=new_category)
+            question.insert()
+
+            selection = Question.query.order_by(Question.id).all()
+            current_questions = paginate_questions(request, selection)
+            
+            return jsonify(
+                {
+                    "success": True,
+                    "created": question.id,
+                    "questions": current_questions,
+                    "total_questions": len(Question.query.all())
+                }
+            )
+
+        except:
+            abort(422)
+
+    """
     TEST: When you submit a question on the "Add" tab,
     the form will clear and the question will appear at the end of the last page
     of the questions list in the "List" tab.
@@ -65,7 +187,25 @@ def create_app(test_config=None):
     Create a POST endpoint to get questions based on a search term.
     It should return any questions for whom the search term
     is a substring of the question.
+    """
+    @app.route('/questions/search', methods=['POST'])
+    def search_questions():
+        body = request.get_json()
+        search_term = body.get('searchTerm', None)
 
+        if search_term:
+            search_results = Question.query.filter(Question.question.ilike(f'%{search_term}%')).all()
+
+            return jsonify({
+                'success': True,
+                'questions': [question.format() for question in search_results],
+                'total_questions': len(search_results),
+                'current_category': None
+            })
+
+        abort(404)
+
+    """
     TEST: Search by any phrase. The questions list will update to include
     only question that include that string within their question.
     Try using the word "title" to start.
@@ -79,6 +219,26 @@ def create_app(test_config=None):
     categories in the left column will cause only questions of that
     category to be shown.
     """
+    @app.route("/categories/<int:id>/questions")
+    def questions_in_category(id):
+
+        # get the category by given id
+        category = Category.query.filter_by(id=id).one_or_none()
+
+        if category:
+            # get all questions in a category
+            questions_in_category = Question.query.filter_by(category=str(id)).all()
+            current_questions = paginate_questions(request, questions_in_category)
+
+            return jsonify({
+                'success': True,
+                'questions': current_questions,
+                'total_questions': len(questions_in_category),
+                'current_category': category.type
+            })
+        
+        else:
+            abort(404)
 
     """
     @TODO:
@@ -86,7 +246,36 @@ def create_app(test_config=None):
     This endpoint should take category and previous question parameters
     and return a random questions within the given category,
     if provided, and that is not one of the previous questions.
+    """
+    @app.route('/quizzes', methods=['POST'])
+    def play_quiz():
 
+        try:
+
+            body = request.get_json()
+
+            if not ('quiz_category' in body and 'previous_questions' in body):
+                abort(422)
+
+            category = body.get('quiz_category')
+            previous_questions = body.get('previous_questions')
+
+            if category['type'] == 'click':
+                available_questions = Question.query.filter(
+                    Question.id.notin_((previous_questions))).all()
+            else:
+                available_questions = Question.query.filter_by(category=category['id']).filter(Question.id.notin_((previous_questions))).all()
+
+            new_question = available_questions[random.randrange(0, len(available_questions))].format() if len(available_questions) > 0 else None
+
+            return jsonify({
+                'success': True,
+                'question': new_question
+            })
+        except:
+            abort(422)
+
+    """
     TEST: In the "Play" tab, after a user selects "All" or a category,
     one question at a time is displayed, the user is allowed to answer
     and shown whether they were correct or not.
@@ -97,6 +286,37 @@ def create_app(test_config=None):
     Create error handlers for all expected errors
     including 404 and 422.
     """
+    @app.errorhandler(400)
+    def bad_request_error(error):
+        return jsonify({
+            "success": False,
+            "error": 400,
+            "message": "Bad request"
+        }), 400
+
+    @app.errorhandler(404)
+    def not_found_error(error):
+        return jsonify({
+            "success": False,
+            "error": 404,
+            "message": "Question not found!"
+        }), 404
+
+    @app.errorhandler(422)
+    def unprocessable_error(error):
+        return jsonify({
+            "success": False,
+            "error": 422,
+            "message": "Request was unprocessable"
+        }), 422
+
+    @app.errorhandler(500)
+    def server_error(error):
+        return jsonify({
+            "success": False,
+            "error": 500,
+            "message": "Server Error"
+        }), 500
 
     return app
 
